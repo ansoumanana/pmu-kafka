36c1f3dd6e8a6c369365c75a7fc7df08975d7385
acisse <ansoumana.cisse@photobox.com>
2022-12-04 10:58:01 +0100

    PMU : init kafka repos

---
+++ create-kafka-topics.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo
+echo "Create topic mysql.pmu_course.course"
+echo "------------------------------------"
+docker exec -t zookeeper kafka-topics --create --bootstrap-server kafka:9092 --replication-factor 1 --partitions 5 --topic mysql.pmu_course.course
+
+echo
+echo "Create topic mysql.pmu_course.partant"
+echo "------------------------------------"
+docker exec -t zookeeper kafka-topics --create --bootstrap-server kafka:9092 --replication-factor 1 --partitions 5 --topic mysql.pmu_course.partant
+
+echo
+echo "List topics"
+echo "-----------"
+docker exec -t zookeeper kafka-topics --list --bootstrap-server kafka:9092
